7aa0469b090830f5ec0b18dee411bf2859be451b
AdamAksel <akseltheaustrian@gmail.com>
2021-02-05 18:40:58 +0700

    hamburger menu

--- frontend/src/components/Navbar/Navbar.elements.js
+++ frontend/src/components/Navbar/Navbar.elements.js
@@ -27,4 +27,40 @@ export const NavText = styled.div`
     color: deepskyblue;
     cursor: pointer;
   }
+
+  @media (max-width: 600px) {
+    font-size: 1rem;
+  }
+`
+
+export const NavHamburger = styled.div`
+  position: absolute;
+  top: 1rem;
+  right: 0;
+  height: 3rem;
+  width: 3rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media (min-width: 600px) {
+    display: none;
+  }
+`
+export const NavHamContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #404040;
+`
+export const NavHamWrapper = styled.div`
+  position: absolute;
+  top: 2rem;
+  right: 5vw;
+
+  @media (min-width: 600px) {
+    display: none;
+  }
 `
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,17 +1,56 @@
-import React from 'react'
-import { NavWrapper, NavText, NavContainer } from './Navbar.elements'
+import React, { useState } from 'react'
+import {
+  NavWrapper,
+  NavText,
+  NavContainer,
+  NavHamburger,
+  NavHamContainer,
+  NavHamWrapper,
+} from './Navbar.elements'
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(0)
+
+  const navOpenFunction = () => {
+    if (navOpen === 0) {
+      setNavOpen(1)
+    } else {
+      setNavOpen(0)
+    }
+  }
+
   return (
-    <NavContainer>
-      <NavWrapper>
-        <NavText>FAQ</NavText>
-        <NavText>Projects</NavText>
-        <NavText>Contact</NavText>
-        <NavText>Blog</NavText>
-        <NavText>Movies&Music</NavText>
-      </NavWrapper>
-    </NavContainer>
+    <>
+      <NavContainer>
+        <NavWrapper>
+          <NavText>FAQ</NavText>
+          <NavText>Projects</NavText>
+          <NavText>Contact</NavText>
+          <NavText>Blog</NavText>
+          <NavText>Movies&Music</NavText>
+        </NavWrapper>
+      </NavContainer>
+      {navOpen === 0 ? (
+        <NavHamburger onClick={navOpenFunction}>
+          <i class='fas fa-hamburger'></i>
+        </NavHamburger>
+      ) : (
+        <>
+          <NavHamburger onClick={navOpenFunction}>
+            <i class='fas fa-hamburger'></i>
+          </NavHamburger>{' '}
+          <NavHamWrapper>
+            <NavHamContainer>
+              <NavText>FAQ</NavText>
+              <NavText>Projects</NavText>
+              <NavText>Contact</NavText>
+              <NavText>Blog</NavText>
+              <NavText>Movies&Music</NavText>
+            </NavHamContainer>
+          </NavHamWrapper>{' '}
+        </>
+      )}
+    </>
   )
 }
 
