fc2e3487adb3426f3a31ca10c030719c5c79d0c1
AdamAksel <akseltheaustrian@gmail.com>
2021-02-20 23:44:50 +0700

    first try!

--- frontend/src/components/Login/Login.elements.js
+++ frontend/src/components/Login/Login.elements.js
@@ -14,3 +14,44 @@ export const LoginArea = styled.div`
   width: 100vw;
   min-height: 85vh;
 `
+
+export const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 1000;
+`
+
+export const ModalArea = styled.div`
+  position: absolute;
+  top: 5vh;
+  left: 5vw;
+  width: 90vw;
+  height: 90vh;
+  background-color: white;
+  z-index: 1001;
+`
+
+export const ModalLeft = styled.div`
+  position: absolute;
+  left: 5vw;
+  top: 5vh;
+  height: 85vh;
+  width: 40vw;
+  overflow: scroll;
+`
+export const ModalRight = styled.div`
+  position: absolute;
+  left: 50vw;
+  top: 5vh;
+  height: 85vh;
+  width: 40vw;
+  overflow: scroll;
+`
+
+export const TextArea = styled.textarea`
+  overflow: scroll;
+`
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
-import { LoginWrapper, LoginArea } from './Login.elements'
-import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import {
+  LoginWrapper,
+  LoginArea,
+  ModalBackground,
+  ModalArea,
+  ModalLeft,
+  ModalRight,
+  TextArea,
+} from './Login.elements'
+import { useDispatch, useSelector } from 'react-redux'
 import { admin } from '../../redux/actions'
+import axios from 'axios'
 
 const Login = () => {
-  //console.log(process.env.REACT_APP_KEY)
-
+  const access = useSelector(state => state.admin)
   var [pass, setPass] = useState()
   var [passKey, setPassKey] = useState()
+  var [FAQModal, setFAQModal] = useState(false)
+  var [blogModal, setBlogModal] = useState(false)
+  var [projectsModal, setProjectsModal] = useState(false)
+  var [modal, setModal] = useState(false)
+  var [FAQ, setFAQ] = useState([])
+  var [FAQQuestion, setFAQQuestion] = useState()
+  var [FAQAnswer, setFAQAnswer] = useState()
+
+  useEffect(() => {
+    axios
+      .get('/api/FAQ')
+      .then(res => {
+        setFAQ(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   const dispatch = useDispatch()
 
@@ -19,44 +45,127 @@ const Login = () => {
     setPass((pass = event.target.value))
   }
 
+  function postFAQQuestionHandler(e) {
+    setFAQQuestion(e.target.value)
+  }
+  function postFAQAnswerHandler(e) {
+    setFAQAnswer(e.target.value)
+  }
+  function PostFAQ(e) {
+    e.preventDefault()
+    const data = {
+      question: FAQQuestion,
+      answer: FAQAnswer,
+    }
+    axios
+      .post('/api/FAQ', data)
+      .then()
+      .catch(err => console.log(err))
+  }
+
   const submitHandler = e => {
     e.preventDefault()
     if (pass === process.env.REACT_APP_KEY) {
-      dispatch(admin(passKey))
+      let passNumeral = parseInt(passKey, 10)
+      dispatch(admin(passNumeral))
+    }
+    if (access === 9000) {
+      setFAQModal(true)
+      setBlogModal(false)
+      setProjectsModal(false)
+    } else if (access === 9001) {
+      setFAQModal(false)
+      setBlogModal(true)
+      setProjectsModal(false)
+    } else if (access === 9002) {
+      setFAQModal(false)
+      setBlogModal(false)
+      setProjectsModal(true)
     }
   }
 
-  return (
-    <LoginWrapper>
-      <LoginArea>
-        <form>
-          <label>
-            Are you the KeyMaster? <br />
-            <input
-              type='text'
-              id='key'
-              name='key'
-              onChange={handleChange}
-            ></input>
-          </label>{' '}
-          <br />
-          <label>
-            How many Keys? <br />
-            <input
-              type='text'
-              id='key1'
-              name='key1'
-              onChange={handleChangeAgain}
-            ></input>
-          </label>{' '}
-          <br />
-          <button type='submit' value='submit' onClick={submitHandler}>
-            Yes!
-          </button>
-        </form>
-      </LoginArea>
-    </LoginWrapper>
-  )
+  const modalHandler = () => {
+    if (modal) {
+      setModal(false)
+    } else {
+      setModal(true)
+    }
+  }
+
+  console.log(access)
+
+  if (!modal) {
+    return (
+      <LoginWrapper>
+        <LoginArea>
+          <form>
+            <label>
+              Are you the KeyMaster? <br />
+              <input
+                type='text'
+                id='key'
+                name='key'
+                onChange={handleChange}
+              ></input>
+            </label>{' '}
+            <br />
+            <label>
+              How many Keys? <br />
+              <input
+                type='number'
+                id='key1'
+                name='key1'
+                onChange={handleChangeAgain}
+              ></input>
+            </label>{' '}
+            <br />
+            <button type='submit' value='submit' onClick={submitHandler}>
+              Yes!
+            </button>
+          </form>
+
+          <div>
+            {FAQModal ? (
+              <button onClick={modalHandler}>FAQ</button>
+            ) : blogModal ? (
+              <button>Blog</button>
+            ) : projectsModal ? (
+              <button>Projects</button>
+            ) : null}
+          </div>
+        </LoginArea>
+      </LoginWrapper>
+    )
+  } else if (modal && FAQModal) {
+    return (
+      <ModalBackground>
+        <ModalArea>
+          <ModalLeft>
+            {FAQ.map(qAndA => (
+              <ul key={qAndA._id}>
+                <li>
+                  ID: {qAndA._id} <br />
+                  Q: {qAndA.question} <br />
+                  A: {qAndA.answer} <br />
+                  <br />
+                </li>
+              </ul>
+            ))}
+          </ModalLeft>
+          <ModalRight>
+            <div>
+              <h4>Post</h4>
+              <TextArea onChange={postFAQQuestionHandler}></TextArea>
+              <TextArea onChange={postFAQAnswerHandler}></TextArea>
+              <button type='submit' value='submit' onClick={PostFAQ}>
+                Post
+              </button>
+            </div>
+          </ModalRight>
+        </ModalArea>
+      </ModalBackground>
+    )
+  }
 }
 
 export default Login
